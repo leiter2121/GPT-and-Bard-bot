e23d38efb4b7ccf5a7ac24b15dbf5e4a83049c2e
leiter2121 <130805371+leiter2121@users.noreply.github.com>
2023-05-16 17:01:11 -0400

    Add files via upload

---
+++ speak_text.py
@@ -0,0 +1,29 @@
+import boto3
+import pygame
+import tempfile
+
+# Set your Amazon Polly credentials
+polly = boto3.client(
+    "polly",
+    region_name="YOUR AWS REGION",
+    aws_access_key_id="YOUR AWS ACCESS ID",
+    aws_secret_access_key="YOUR AWS SECRET KEY"
+)
+
+# Define function to speak text using Amazon Polly
+def speak_text(text):
+    response = polly.synthesize_speech(
+        Text=text,
+        Engine='neural',
+        OutputFormat='mp3',
+        VoiceId='Joey'
+    )
+    with tempfile.TemporaryFile() as temp_file:
+        temp_file.write(response["AudioStream"].read())
+        temp_file.flush()
+        temp_file.seek(0)
+        pygame.mixer.init()
+        pygame.mixer.music.load(temp_file)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            continue
